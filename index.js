7b4bedfa0e506fe5e7ee66afc2bb2aab0a8ebcfa
climba <mike@joomla-go.com>
2018-05-21 23:34:14 -0700

    Added some base files and gitignore

---
+++ index.js
@@ -0,0 +1,33 @@
+// Require the necessary files and packages
+var Word = require('./word.js');
+var inquirer = require('inquirer');
+var randomWords = require('random-words');
+
+// console.log(randomWords());
+
+// console.log(randomWords({exactly:30, wordsPerString:1, formatter: (word, index)=> {
+//   return index === 0 ? word.slice(0,1).toLowerCase().concat(word.slice(1)) : word;
+// }}));
+
+// Array to hold all the possible words to guess
+var guessWords = [ 'Inside','Crack','Mainly','Pleasant','Outside','Equipment','Government','Came','Underline','May','Nine','Enter','Love','Happily','Difference','Describe','Experience','Itself','Enough','Smooth','Trace','Writer','Identity','Any','Yesterday','Military','Consonant','Shadow','Somebody','Brick' ]
+
+// Get the number of words in the array
+var arrLength = guessWords.length;
+
+console.log(arrLength);
+
+// Math function to get length of array and times it by a random number
+
+
+
+
+
+// Use random number to pick the word
+
+
+// Once word is selected loop through it and count how many letters are in that word
+
+
+
+// Print out the correct number of underscores according to the words length
