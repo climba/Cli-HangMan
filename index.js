96f3e42d4d19ae9fd87a48d2ca1ec146605f69e4
climba <mike@joomla-go.com>
2018-05-22 15:53:53 -0700

    Added some new functions

--- index.js
+++ index.js
@@ -4,23 +4,25 @@ var inquirer = require('inquirer');
 var randomWords = require('random-words');
 var random = require('lodash.random');
 
-// console.log(randomWords());
 
-// console.log(randomWords({exactly:30, wordsPerString:1, formatter: (word, index)=> {
-//   return index === 0 ? word.slice(0,1).toLowerCase().concat(word.slice(1)) : word;
-// }}));
+var rando = randomWords({exactly:30, wordsPerString:1, formatter: (word, index)=> {
+  return index === 0 ? word.slice(0,1).toLowerCase().concat(word.slice(1)) : word;
+}})
+
+// console.log(rando);
 
 // Array to hold all the possible words to guess
-var guessWords = [ 'Inside','Crack','Mainly','Pleasant','Outside','Equipment','Government','Came','Underline','May','Nine','Enter','Love','Happily','Difference','Describe','Experience','Itself','Enough','Smooth','Trace','Writer','Identity','Any','Yesterday','Military','Consonant','Shadow','Somebody','Brick' ]
+// var guessWords = [ 'Inside','Crack','Mainly','Pleasant','Outside','Equipment','Government','Came','Underline','May','Nine','Enter','Love','Happily','Difference','Describe','Experience','Itself','Enough','Smooth','Trace','Writer','Identity','Any','Yesterday','Military','Consonant','Shadow','Somebody','Brick' ]
 
 // Get the number of words in the array
-var arrLength = guessWords.length;
+var arrLength = rando.length;
 
 console.log(arrLength);
 
 // Math function to get length of array and times it by a random number
+var wordToGuess = rando[Math.floor(Math.random() * rando.length)];
 
-
+console.log(wordToGuess);
 
 
 
